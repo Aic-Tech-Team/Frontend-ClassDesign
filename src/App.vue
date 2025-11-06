a283127cfc26c42df6d8eb5738de83f6f5c1d482
Benyamin Keshavarz <beny.keshavarz2018@gmail.com>
2025-11-06 11:15:03 +0330

    [INITIALIZE] Initialize project

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { RouterView } from "vue-router";
+import TargetCursor from "./blocks/TargetCursor/TargetCursor.vue";
+
+import { useMediaQuery } from "@vueuse/core";
+
+const isLargeScreen = useMediaQuery("(min-width: 768px)");
+</script>
+
+<template>
+  <RouterView />
+  <TargetCursor
+    v-if="isLargeScreen"
+    :spin-duration="5"
+    :hide-default-cursor="true"
+  />
+</template>
---
+++ src/components/Hero.vue
@@ -0,0 +1,38 @@
+<template>
+  <header
+    class="relative isolate w-full h-dvh bg-transparent flex-col-center pointer-events-none"
+  >
+    <img
+      src="../assets/images/AIC_logo_notext_white.svg"
+      alt="aic logo"
+      class="w-28 sm:w-36 absolute top-10 left-1/2 -translate-x-1/2"
+    />
+
+    <TextType
+      :text="['کلاس 311']"
+      :typingSpeed="75"
+      :pauseDuration="1500"
+      :show-cursor="false"
+      class="text-6xl sm:text-7xl lg:text-8xl xl:text-9xl font-black text-center"
+    />
+
+    <a
+      href="#spotlight-card"
+      class="cursor-target pointer-events-auto cursor-none border-2 mt-8 sm:mt-12 border-white rounded-full px-10 pt-2 pb-3 text-white text-xl lg:text-2xl"
+    >
+      بزن بریم
+    </a>
+
+    <div
+      class="flex-row-center absolute bottom-20 start-10 rotate-90 gap-2 translate-y-20 max-lg:translate-x-28"
+    >
+      <div class="bg-linear-to-r from-white to-transparent h-px w-48 sm:w-60" />
+      <p class="text-white sm:text-lg whitespace-nowrap">بیا پایین</p>
+    </div>
+  </header>
+</template>
+
+<script setup lang="ts">
+import TextType from "../blocks/TextType/TextType.vue";
+// import Particles from "../blocks/Particles/Particles.vue";
+</script>
